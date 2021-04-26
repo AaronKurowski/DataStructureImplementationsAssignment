4de75f45da7e94d150ba88d118259018b2d3e66f
Aaron Kurowski <aaron12342@att.net>
2021-04-26 17:07:12 -0500

    Created a method to add a node to the beginning of the linked list.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from data1 import Data1
+from linkedlist import LinkedList
 
 
 if __name__ == '__main__':
-    data = Data1()
+    # data = Data1()
     # data.print_pi_day_month()
 
     # data.print_bday_locations()
     # data.sweepstakes_winner()
 
-    data.print_brothers()
+    # data.print_brothers()
+
+    linked_list = LinkedList()
+    linked_list.append_node(55)
+    linked_list.append_node(60)
+    linked_list.append_node(65)
+    linked_list.add_node_to_beginning(50)
+    print(linked_list.head.data)
+
+    linked_list.add_node_to_beginning(45)
+    print(linked_list.head.data)
+    print(linked_list.tail.data)
